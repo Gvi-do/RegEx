d65b799b3d093c67847254187f6a686f71ebb3d0
Евгений <zetkornev@gmail.com>
2020-11-07 17:22:25 +0300

    change def phone_format

--- main.py
+++ main.py
@@ -8,25 +8,13 @@ with open("phonebook_raw.csv", encoding='utf-8') as f:
   contacts_list = list(rows)
 
 def phone_format(contacts):
-    count = 0
     for list in contacts:
       if 'доб.' in list[5]:
         pattern1 = re.compile(r'[+7|8]+[ (]+(\d+)[ )-]+(\d+)[ -](\d+)[ -](\d+)[ |(]+(доб\.)[ ](\d+)[)]?')
         list[5] = pattern1.sub(r'+7(\1)\2-\3-\4 \5\6', list[5])
-        count +=1
-
-      elif count == 2:
-        pattern2 = re.compile(r'[+7|8]+(\d{3})(\d{3})(\d{2})(\d{2})')
-        list[5] = pattern2.sub(r'+7(\1)\2-\3-\4', list[5])
-        count += 1
-      elif count == 3:
-        pattern3 = re.compile(r'[+7|8]+ (\d{3})-(\d{3})-(\d{2})(\d{2})')
-        list[5] = pattern3.sub(r'+7(\1)\2-\3-\4', list[5])
-        count +=1
       else:
-        pattern = re.compile(r'[+7|8]+[ (]+(\d+)[ )-]+(\d+)[ -](\d+)[ -](\d+)')
+        pattern = re.compile(r'[+7|8]*[ |(]*(\d{3})[ |)|-]*(\d{3})[ |-]?(\d{2})[ |-]?(\d{2})')
         list[5] = pattern.sub(r'+7(\1)\2-\3-\4', list[5])
-        count += 1
 
 def name_format(contacts):
     for contact in contacts[1:]:
